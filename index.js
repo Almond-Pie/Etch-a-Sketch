75ea6f1142852d1b0f1e3909ca642c5c1a592830
Almond-Pie <jelanigaming@gmail.com>
2025-03-05 13:44:31 +0000

    Deleted CSS styling in favour of using JS
    •Removed: :hover from stylesheet
    •Added: EventListener "mouseover"
    •Added: function() to create a function in event listener that changes color of the div.
    •Learned: div.style to style elements in js.

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
+// Promt user for the size of grid 
 let height = window.prompt("Wat do you what the height to be ?, ")
 let width = window.prompt("What do you what the width to be ?, ")
 
+// Target the container ID
 const Container  = document.querySelector("#Container ")
 
+// Use a for loop to simplify creating the divs
 for (let i = 0; i < height; i++) {   // Outer loop → rows
     for (let j = 0; j < width; j++) {  // Inner loop → columns
         let div = document.createElement("div"); // Create a new div
         Container.appendChild(div); // Append div to container
     }
-} 
+}
 
+// Use event listener to change color of the div
+div.addEventListener("mouseover", function() {
+    div.style.backgroundColor = "blue";
+});
